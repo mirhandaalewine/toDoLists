ed497f66124b1f0940a42737f75d94147bf98f64
Mirhanda <mhopealewine@gmail.com>
2022-05-02 09:43:59 -0400

    refactored addTodo

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 function App(){
   const [todos, setTodos] = React.useState([
     {
-      text: 'go grocery shopping',
+      text: 'learn react',
       isCompleted: false,
     },
     {
-      text: 'workout',
+      text: 'meet friend for lunch',
       isCompleted: false,
     },
     {
-      text: 'finish homework',
+      text: 'build todo app',
       isCompleted: false,
-    },
-    {
-      text: 'walk dog',
-      isCompleted: false,
-    }              
-  ])
+    }        
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, {text:text, isCompleted:false}];
+    setTodos(newTodos);
+  }
+  
+  const removeTodo = e => {
+    var index = Number(e.target.id);
+    let temp = [...todos];    
+    temp.splice(index, 1);
+    setTodos(temp);
+  }
 
   return(
     <>
-      {todos.map((todo, i) => 
-        <div className ="todo" key={i}>{todo.text}</div>)}
+      {todos.map((todo, i) => (
+        <div className="todo" key={i} id={i} onClick={removeTodo}>{todo.text}</div>
+      ))}
+      <TodoForm addTodo={addTodo} />
     </>
   );
 }
