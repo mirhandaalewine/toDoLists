6ab50eec9071a3a2d34337713eeeb366f4a7dfd5
Mirhanda <mhopealewine@gmail.com>
2022-05-04 08:41:29 -0400

    style changes

--- todo.js
+++ todo.js
@@ -2,5 +2,5 @@ function Todo ({todo, index, remove}) {
     function handle () {
         remove(index);
     }
-    return <div className="todo" onClick={handle}>{todo.text} (-)</div>
+    return <div className="todo" onClick={handle}>{todo.text} (x)</div>
 }
